39e08ece1857a5e45902c4154160dcfadfe7f98e
Illia Bazarni <illia.11baza@gmail.com>
2024-10-22 12:24:50 +0300

    змінено файл роботи пр1

--- pr7.cpp
+++ pr7.cpp
@@ -5,6 +5,4 @@ int main()
     std::cout << "Hello, my name is Illya Bazarniy!\n";
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::cout << "Привіт, мене звати Ілля Базарний!\n";
-    std::cout << 16.0 / 4.0;
 }
