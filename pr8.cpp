a7ede7b4b8510a29cb7aa0e7e13d8f7f24755f8e
Illia Bazarni <illia.11baza@gmail.com>
2024-11-05 19:34:57 +0200

    add new fail

---
+++ pr8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double y, Z;
+    cout << "Введіть значення y: ";
+    cin >> y;
+    double dob = 1;
+    for (int k = 1; k <= 10; k++) {
+        dob *= log(k * y);
+    }
+    Z = sin(dob);
+    cout << "Z = " << Z << endl;
+
+    return 0;
+}
